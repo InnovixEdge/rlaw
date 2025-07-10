244ab4c1c04f77d4f4bffb8d8d61a943d35adcca
InnovixEdge <jay.cadmus@innovix-llc.com>
2025-07-09 23:06:03 -0400

    Create oauth.ts

---
+++ lib/services/oauth.ts
@@ -0,0 +1,10 @@
+// Placeholder OAuth service
+export function googleAuthUrl() {
+  // TODO: Implement Google OAuth URL generation
+  return 'https://accounts.google.com/oauth/authorize';
+}
+
+export function handleGoogleCallback() {
+  // TODO: Implement Google OAuth callback handling
+  return null;
+}
