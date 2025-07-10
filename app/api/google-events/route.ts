b827a805242dba8a2efba6f72d8303ca47464d2d
InnovixEdge <jay.cadmus@innovix-llc.com>
2025-07-09 23:20:18 -0400

    Update route.ts

--- app/api/google-events/route.ts
+++ app/api/google-events/route.ts
@@ -1,4 +1,4 @@
-import { getGoogleCalendarEvents } from '@/backend/services/google-calendar'
+//import { getGoogleCalendarEvents } from '@/backend/services/google-calendar'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
